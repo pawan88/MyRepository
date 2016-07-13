4e66492fa94df3132059b82244294267db298543
Pawan <pawan.mengade@gmail.com>
2016-07-13 11:21:42 +0530

    test changes

---
+++ TestApplication/TestApplication/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TestApplication
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnChkUrl = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lavender;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(9, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(482, 209);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(374, 168);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 27);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(32, 15);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(418, 56);
+            this.panel2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Health Check Utility";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.btnChkUrl);
+            this.panel3.Location = new System.Drawing.Point(32, 77);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(418, 83);
+            this.panel3.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(374, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // btnChkUrl
+            // 
+            this.btnChkUrl.Location = new System.Drawing.Point(322, 42);
+            this.btnChkUrl.Name = "btnChkUrl";
+            this.btnChkUrl.Size = new System.Drawing.Size(75, 23);
+            this.btnChkUrl.TabIndex = 6;
+            this.btnChkUrl.Text = "Check Url";
+            this.btnChkUrl.UseVisualStyleBackColor = true;
+            this.btnChkUrl.Click += new System.EventHandler(this.btnChkUrl_Click);
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(501, 228);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnChkUrl;
+
+
+    }
+}
+
---
+++ TestApplication/TestApplication/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HealthCheckLib;
+using System.Net;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+
+namespace TestApplication
+{
+    public partial class frmHome : Form,HealthCheckLib.IApplicationCheckClass
+    {
+        static string strAppName;
+        string strXmlFilePath;
+        string str;
+
+        public frmHome()
+        {
+            InitializeComponent();        
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            strXmlFilePath = System.Configuration.ConfigurationManager.AppSettings["filePath"].ToString();            
+        }        
+
+        private void btnChkUrl_Click(object sender, EventArgs e)
+        {
+            strAppName = "Test_URL";
+            string strUrl = textBox2.Text.ToLower().Trim();
+
+            if (string.IsNullOrEmpty(strUrl))
+            { 
+                MessageBox.Show("Please enter valid Url");
+                return;
+            }            
+
+            if (strUrl.Contains("http://") == false && strUrl.Contains("https://")==false)
+            {
+                strUrl = "http://" + strUrl;
+                //MessageBox.Show("Invalid Url", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //return;
+            }
+
+            List<ApplicationResultCheck> listResultCheck = new List<ApplicationResultCheck>();    
+   
+            ApplicationResultCheck objCheckClass = check(strUrl);
+            ApplicationResultCheck objCheckClass1 = check(strUrl);
+
+            listResultCheck.Add(objCheckClass);
+            listResultCheck.Add(objCheckClass1);
+            
+            XmlDocument xmlDoc = generateXML(listResultCheck, strAppName, "Processing", "Dev101");
+            xmlDoc.Save(strXmlFilePath);
+            strUrl = string.Empty;
+        }
+
+        public XmlDocument generateXML(List<ApplicationResultCheck> listParameter, string strApplicationName, string strOverallStatus, string strServerName)
+        {
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            XmlElement root = xmlDoc.CreateElement("application_result");
+            root.SetAttribute("name", strApplicationName);
+            root.SetAttribute("status", strOverallStatus);
+            root.SetAttribute("server-name", strServerName);
+            xmlDoc.AppendChild(root);
+
+            foreach (ApplicationResultCheck item in listParameter)
+            {
+                XmlNode userNode = xmlDoc.CreateElement("test_result");
+
+                XmlAttribute xmlAttName = xmlDoc.CreateAttribute("name");
+                xmlAttName.Value = item.strName;
+                userNode.Attributes.Append(xmlAttName);
+
+                XmlAttribute xmlAttStatus = xmlDoc.CreateAttribute("status");
+                xmlAttStatus.Value = item.strStatus;
+                userNode.Attributes.Append(xmlAttStatus);
+
+                XmlAttribute xmlAttMessage = xmlDoc.CreateAttribute("message");
+                xmlAttMessage.Value = item.strMessage;
+                userNode.Attributes.Append(xmlAttMessage);
+
+                XmlAttribute xmlAttElapsedTime = xmlDoc.CreateAttribute("elapsedtime");
+                xmlAttElapsedTime.Value = item.StrElapsedTime;
+                userNode.Attributes.Append(xmlAttElapsedTime);
+
+                root.AppendChild(userNode);
+            }
+
+            return xmlDoc;
+
+        }
+
+
+        public ApplicationResultCheck check()
+        {            
+            ApplicationResultCheck paramList = new ApplicationResultCheck();
+            return paramList;
+        }
+
+        public ApplicationResultCheck check(string toCheckString)
+        {            
+            TimeSpan ts;            
+            DateTime dtFrom = DateTime.Now;
+            HealthCheckLib.ApplicationResultCheck objResultCheck = new ApplicationResultCheck();            
+
+            if (strAppName == "Test_DBCON")
+            {
+                toCheckString = "";
+            }
+            else if (strAppName == "Test_URL")
+            {
+                try
+                {
+                    HttpWebRequest request = HttpWebRequest.Create(toCheckString) as HttpWebRequest;
+                    request.Timeout = 5000;
+                    request.Method = "HEAD";
+
+                    HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+                    int statuscode = (int)response.StatusCode;
+
+                    objResultCheck.strStatus = response.StatusCode.ToString();
+                    objResultCheck.strMessage = "";
+                    objResultCheck.strName = "Test Url";
+
+                }
+                catch (WebException ex)
+                {
+                    objResultCheck.strStatus = "Down";
+                    objResultCheck.strMessage = ex.Message;
+                }
+                catch (Exception ex)
+                {
+                    objResultCheck.strStatus = "Down";
+                    objResultCheck.strMessage = ex.Message;
+                }
+
+                ts = DateTime.Now - dtFrom;
+                objResultCheck.StrElapsedTime = ts.Seconds.ToString();                
+            }
+            else
+            {
+                toCheckString = "";
+            }
+                        
+            return objResultCheck;
+        }
+        
+        public ApplicationResultCheck check(string strParam1, string strParam2)
+        {
+            ApplicationResultCheck paramList = new ApplicationResultCheck();
+            return paramList;
+        }        
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void testXML()
+        {
+            XDocument doc;
+            string filePath = "file.xml";
+            if (!File.Exists(filePath))
+            {
+                doc = new XDocument(new XElement("Parent"));
+                doc.Save(filePath);                
+            }
+
+            // Xml exists now
+            // Adding a child to the xml 
+            doc = XDocument.Load(filePath);
+            doc.Element("Parent").Add(new XElement("Child",
+                                        new XAttribute("Date", DateTime.Now.ToLongDateString() + " "
+                                                        + DateTime.Now.ToShortTimeString()),
+                                        new XAttribute("SeqID", "You ID"),
+                                        new XElement("Project", new XText("Pra")),
+                                        new XElement("Mehtod", new XText("PP")),
+                                        new XElement("Msg", new XText("Pandit"))));
+            doc.Save(filePath);            
+        }
+
+    }
+}
